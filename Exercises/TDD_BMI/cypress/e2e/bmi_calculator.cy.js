55830cb137591eb3118fd26a7a89951073ff724c
Christian Gawron <gawron.christian@fh-swf.de>
2023-11-09 11:01:05 +0000

    wip: sample solution - Adding test cases - Implementing frontend

--- Exercises/TDD_BMI/cypress/e2e/bmi_calculator.cy.js
+++ Exercises/TDD_BMI/cypress/e2e/bmi_calculator.cy.js
@@ -4,4 +4,21 @@ describe('BMI Calculator', function () {
     it('successfully loads', function () {
         cy.visit(`http://localhost:${PORT}`) // ändern Sie URL zu Ihrer App
     })
+
+    it('has the correct title', function () {
+        cy.visit(`http://localhost:${PORT}`) // ändern Sie URL zu Ihrer App
+        cy.title().should('include', 'BMI Rechner')
+    });
+
+    it('has the correct heading', function () {
+        cy.visit(`http://localhost:${PORT}`) // ändern Sie URL zu Ihrer App
+        cy.get('h1').should('contain', 'BMI Rechner')
+    });
+
+    it('shows the correct BMI value', function () {
+        cy.visit(`http://localhost:${PORT}`) // ändern Sie URL zu Ihrer App
+        cy.get('#weight').as('range').invoke('val', 76).trigger('change')
+        cy.get('#height').as('range').invoke('val', 181).trigger('change')
+        cy.get('#bmi').should('contain', '23.2')
+    })
 })
--- Exercises/TDD_BMI/frontend/main.js
+++ Exercises/TDD_BMI/frontend/main.js
@@ -1,5 +1,6 @@
 Array.from(document.getElementsByTagName('input')).forEach(element => {
     element.addEventListener('change', updateUI);
+    element.addEventListener('input', updateUI);
 });
 
 function updateUI() {
@@ -8,10 +9,13 @@ function updateUI() {
     const height = document.getElementById('height').value;
     const bmi = calculateBMI(weight, height);
     console.log("weight: %s, height: %s, bmi: %s", weight, height, bmi);
+    document.getElementById('bmi').innerText = bmi;
 }
 
 function calculateBMI(weight, height) {
     // Logik zur Berechnung des BMI hier
+    height = height / 100;
+    return weight / (height * height);
 }
 
 updateUI();
