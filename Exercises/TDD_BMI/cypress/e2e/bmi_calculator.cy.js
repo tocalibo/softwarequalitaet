6aea741056186518a91c3d4eadfde6eaa407fcdf
tocalibo <91373752+tocalibo@users.noreply.github.com>
2024-10-19 13:35:22 +0000

    Pending changes exported from your codespace

--- Exercises/TDD_BMI/cypress/e2e/bmi_calculator.cy.js
+++ Exercises/TDD_BMI/cypress/e2e/bmi_calculator.cy.js
@@ -5,3 +5,31 @@ describe('BMI Calculator', function () {
         cy.visit(`http://localhost:${PORT}`) // ändern Sie URL zu Ihrer App
     })
 })
+
+describe('Slider Integration Test', () => {
+    const sliderSelector = '#weight';
+    const inputSelector = '#weight_input';
+  
+    it('should update input field when slider value changes', () => {
+      // Visit the page
+      cy.visit(`http://localhost:${PORT}`);
+  
+      // Get the slider and input field
+      cy.get(sliderSelector).as('slider');
+      cy.get(inputSelector).as('inputField');
+  
+      // Change the slider value
+      cy.get('@slider').invoke('val', 50).trigger('input');
+  
+      // Verify the input field value changes accordingly
+      cy.get('@inputField').should('have.value', '50');
+    });
+  });
+
+describe('BMI no Input Test', () => {
+    it('should display "Bitte geben Sie gültige Werte ein." when no input', () => {
+        cy.visit(`http://localhost:${PORT}`);
+        cy.contains('BMI Berechnen').click();
+        cy.get('#result').should('have.text', 'Bitte geben Sie gültige Werte ein.');
+    });
+});
